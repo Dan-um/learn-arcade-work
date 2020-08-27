e89ad4f23f42eb0ca4b4aeb56690b37d2780788d
Daniel DeLaria <daniel.delaria@my.simpson.edu>
2020-08-27 15:38:19 -0500

    Work on Lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-print("hello")
+print("Jon and Jim went to the store")
+print("to buy a loaf of bread")
+print("they bought a four month old loaf")
+print("Jon said:\"this bread is old\" Jim said \"this bread has mold\" ")
