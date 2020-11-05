4534d9c8e454cf810f524ad0f7fd8f1e57aef429
Daniel DeLaria <daniel.delaria@my.simpson.edu>
2020-11-04 20:35:18 -0600

    lab 10 resubmission

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -23,8 +23,46 @@ def main():
 
     print("---Linear Search---")
     my_file2 = open("AliceInWonderLand200.txt")
-    for  in my_file2:
+    current_line = 0
+    for line in my_file2:
         word_list = split_line(line)
+        current_line += 1
+        for word in word_list:
+            key = word.upper()
+            current_list_position = 0
+            while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != key:
+                current_list_position += 1
+            if current_list_position == len(dictionary_list):
+                print("Line", current_line, word)
+
+    print("---Binary Search---")
+    my_file2 = open("AliceInWonderLand200.txt")
+    current_line = 0
+    for line in my_file2:
+        word_list = split_line(line)
+        current_line += 1
+        for word in word_list:
+            key = word.upper()
+            lower_bound = 0
+            upper_bound = len(dictionary_list)-1
+            found = False
+            while lower_bound <= upper_bound and not found:
+                middle_pos = (lower_bound + upper_bound) // 2
+                if dictionary_list[middle_pos] < key:
+                    lower_bound = middle_pos + 1
+                elif dictionary_list[middle_pos] > key:
+                    upper_bound = middle_pos - 1
+                else:
+                    found = True
+            if not found:
+                print("line", current_line, word)
+
+
+
+
+
+
+
 
 
 
